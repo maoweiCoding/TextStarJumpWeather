f3715b89106c3fba38419d7e5c13e84824a3e7f9
maowei <1543235393@qq.com>
2022-05-03 22:14:44 +0800

    实现搜索全球城市数据功能。

---
+++ app/src/main/java/com/starjumpweather/android/logic/Repository.kt
@@ -0,0 +1,27 @@
+package com.starjumpweather.android.logic
+
+import androidx.lifecycle.liveData
+import com.starjumpweather.android.logic.model.Place
+import com.starjumpweather.android.logic.network.StarJumpWeatherNetwork
+import kotlinx.coroutines.Dispatchers
+import java.lang.Exception
+
+object Repository {
+    fun searchPlaces(query:String) = liveData(Dispatchers.IO){
+        val result = try {
+            val placeResponse = StarJumpWeatherNetwork.searchPlaces(query)
+            if (placeResponse.status=="ok"){
+                val places = placeResponse.places
+                Result.success(places)
+            }
+            else{
+                Result.failure(RuntimeException("response status is ${placeResponse.status}"))
+            }
+        } catch (e:Exception){
+            Result.failure<List<Place>>(e)
+        }
+        emit(result)
+    }
+
+    }
+
